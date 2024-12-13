fa98887141d1fc18a7c5cfa6fc3204a3e01ca303
viitones <vitonesmartins@gmail.com>
2024-12-13 04:40:18 -0300

    desfrutare

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+function closeNav() {
+  document.getElementById('mynav').style.width = '0'
+}
+function openNav() {
+  document.getElementById('mynav').style.width = '100%'
+}
+
+new Swiper('.card-wrapper', {
+  loop: true,
+  spaceBetween: 30,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    900: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+});
