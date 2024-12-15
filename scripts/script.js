33058efb6bed277e4b34630784804d7aa1e63cfd
viitones <vitonesmartins@gmail.com>
2024-12-14 22:47:36 -0300

    updates

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,9 @@ function closeNav() {
   document.getElementById('mynav').style.width = '0'
 }
 function openNav() {
-  document.getElementById('mynav').style.width = '100%'
+  document.getElementById('mynav').style.width = '40%'
 }
+
 
 new Swiper('.card-wrapper', {
   loop: true,
